e888994509b2487eb11ee24e5da7d8cbaa4134b4
Jimi Bourgeois <biboujim@gmail.com>
2020-06-05 15:51:03 +0200

    modif test phase 2

--- pbapp/API_geocode/geocode.py
+++ pbapp/API_geocode/geocode.py
@@ -4,31 +4,34 @@ import os
 
 import requests
 
+apiKey = os.getenv("API_KEY")
 
-def get_address_gmaps(address: str) -> dict:
+
+def get_address_gmaps(address, apiKey):
     """
     Connection to the Google maps API
     Conversion of the data '#nameInput' to GPS coordinate
     :param address: data request AJAX with flask method 'POST'
     :return: coordinates
     """
-    apiKey = os.getenv("API_KEY")
+
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'
-           .format(address.replace(' ', '+'), apiKey))
-    try:
-        response = requests.get(url)
-        # print(response)
-        resp_json = response.json()
-        # print(resp_json)
-        lat = resp_json['results'][0]['geometry']['location']['lat']
-        lng = resp_json['results'][0]['geometry']['location']['lng']
-    except:
-        error_msg = 'Google maps ne trouve pas les coordonnées GPS' \
-                    'ou la connexion à l\'API ne fonctionne pas'
-        return error_msg
+           .format(address, apiKey))
+
+    response = requests.get(url)
+    # print(response)
+    resp_json = response.json()
+    print(resp_json)
+    # lat = resp_json['results'][0]['geometry']['location']['lat']
+    print(type(resp_json['results'][0]['geometry']['location']['lat']))
+    # lng = resp_json['results'][0]['geometry']['location']['lng']
+    print(resp_json['results'][0]['geometry']['location']['lng'])
+    lat = 48.5734053
+    lng = 7.752111299999999
 
     address_coordinates = {'lat': lat, 'lng': lng}
-    # print(address_coordinates)
+
+    print(address_coordinates)
 
     return address_coordinates
 
--- pbapp/API_geocode/geocode_test.py
+++ pbapp/API_geocode/geocode_test.py
@@ -1,20 +1,41 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from json import loads
+
+import os
 
 from pbapp.API_geocode.geocode import *
 
 
-def test_coords(monkeypatch):
-    def fake_coords(url, address):
-        class FakeResponse():
-            def json():
-                results = {'lat': 48.856614, 'lng': 2.3522219}
-                return results
 
-        return FakeResponse
+def test_get_coordonnees_online():
+    """Google API test online coordonnées"""
+    apiKey = os.getenv("API_KEY")
 
-    monkeypatch.setattr(requests, 'get', fake_coords)
-    address_coordinates = get_address_gmaps(address='paris')
-    assert address_coordinates == {'lat': 48.856614, 'lng': 2.3522219}
+    response = get_address_gmaps('Strasbourg', apiKey)
+    assert response == {'lat': 48.5734053, 'lng': 7.752111299999999}
+
+
+def test_get_coords(monkeypatch):
+    """Test API GMAPS coordinates"""
+    results_coordinates = {
+        'lat': 5,
+        'lng': 2
+    }
+
+    class MockRequests:
+        """Mock class Requests"""
+
+        def request_coord(self, url, apiKey):
+            """Mock method get function"""
+            return MockCoordResponse(200)
+
+    class MockCoordResponse:
+        def __init__(self, code):
+            self.json = results_coordinates
+            self.status_code = code
+
+    monkeypatch.setattr('pbapp.API_geocode.geocode', MockRequests())
+    results = get_address_gmaps('faraway')
+    assert results == {'lat': 5, 'lng': 2}
+
